3b34e1029937fce46a065eec3a608f44f7095866
JuanmaRuizF <juanmaruizf@gmail.com>
2022-01-27 01:35:21 +0000

    error page

--- src/Components/error_page.js
+++ src/Components/error_page.js
@@ -1,9 +1,30 @@
+import "../Styles/error_page.css";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Navbar from "./Navbar_components/Navbar";
+import Sidebar from "./Navbar_components/Sidebar";
+import Submenu from "./Navbar_components/Submenu";
 const Error = () => {
   return (
-    <main style={{ padding: "1rem 0" }}>
-      <h2>Error page</h2>
-    </main>
+    <>
+      <div className="error_page">
+        <Navbar />
+        <Sidebar />
+        <Submenu />
+        <div className="align_center">
+          <div>This page doesn't exist</div>
+          <div>:(</div>
+          <div>
+            <Link to="/">
+              <Button variant="secondary" className="home_page">
+                Home page
+              </Button>{" "}
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default Error;
